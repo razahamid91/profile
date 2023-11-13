b0ca8804ea7b7a6e14fadd33ced674646b4c5e58
Hamid Raza <74106196+razahamid91@users.noreply.github.com>
2023-11-13 13:46:53 +0530

    profile assigment completed

--- src/components/ProfileDesign.jsx
+++ src/components/ProfileDesign.jsx
@@ -6,7 +6,7 @@ const ProfileDesign = () => {
                 <div className="photo-intro gap-3 flex">
                     <div className="profile-photo border-2 -my-3 sm:-my-5 md:-my-12 border-gray-500 rounded-full w-16 h-16 sm:w-24 sm:h-24 md:w-44 md:h-44"></div>
                     <div className="profile-name py-2 mx-0 sm:mx-1 md:mx-2 flex flex-col gap-3 justify-start items-between">
-                        <span className="name flex gap-2 justify-center items-center w-fit font-bold text-xl sm:text-2xl md:text-3xl">Abqari Abbas<i className="fa-regular fa-gem flex justify-center items-center p-1 text-xs sm:text-sm bg-purple-900 text-white w-5 h-5 rounded-full"></i><i className="fa-solid fa-check flex justify-center items-center p-1 text-xs sm:text-sm bg-green-400 text-black font-bold w-5 h-5 rounded-full"></i></span>
+                        <span className="name flex gap-2 justify-center items-center w-fit font-bold text-xl sm:text-2xl md:text-3xl">Anuj Gosalia<i className="fa-regular fa-gem flex justify-center items-center p-1 text-xs sm:text-sm bg-purple-900 text-white w-5 h-5 rounded-full"></i><i className="fa-solid fa-check flex justify-center items-center p-1 text-xs sm:text-sm bg-green-400 text-black font-bold w-5 h-5 rounded-full"></i></span>
                         <div className="follow-details flex gap-4 font-bold text-base sm:text-md md:text-lg">
                             <div className="followers flex flex-col">
                                 <span className="followers rounded-lg text-center px-2 sm:px-4 sm:py-1 border-2 text-gray-400 border-gray-400">6482</span>
